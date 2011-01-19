5bff12578f8d629b4463e6051cbb4897d6586031
Johannes Raggam <raggam-nl@adm.at>
2011-01-19 15:45:27 +0100

    update tests for new api

--- src/node/ext/uml/activities-testmodel.py
+++ src/node/ext/uml/activities-testmodel.py
@@ -1,56 +1,48 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright 2009: Johannes Raggam, BlueDynamics Alliance
-#                 http://bluedynamics.com
-# GNU Lesser General Public License Version 2 or later
+import node.ext.uml.core as core
+import node.ext.uml.activities as activities
 
-__author__ = """Johannes Raggam <johannes@raggam.co.at>"""
-__docformat__ = 'plaintext'
+profile = core.Profile('pr')
+#profile['execution1'] = core.Stereotype()
+#profile['execution1']['tgv'] = core.TaggedValue()
 
-import activities.metamodel as mm
-
-profile = mm.Profile('pr')
-#profile['execution1'] = mm.Stereotype()
-#profile['execution1']['tgv'] = mm.TaggedValue()
-
-model = mm.Package('testmodel')
+model = core.Package('testmodel')
 model[profile.__name__] = profile
-model['main'] = mm.Activity()
+model['main'] = activities.Activity()
 act = model['main']
 
-act['pc1'] = mm.PreConstraint(specification='True is True')
-act['po1'] = mm.PostConstraint(specification='False is False')
+act['pc1'] = activities.PreConstraint(specification='True is True')
+act['po1'] = activities.PostConstraint(specification='False is False')
 
-act['start'] = mm.InitialNode()
-act['fork'] = mm.ForkNode()
+act['start'] = activities.InitialNode()
+act['fork'] = activities.ForkNode()
 
-act['action1'] = mm.OpaqueAction()
-act['action1']['execution1'] = mm.Stereotype(profile=profile)
-act['action1']['execution1']['tgv'] = mm.TaggedValue(value="dummy value")
-act['action1']['lpc1'] = mm.PreConstraint(specification='True is True')
-act['action1']['lpo1'] = mm.PostConstraint(specification='False is False')
+act['action1'] = activities.OpaqueAction()
+act['action1']['execution1'] = core.Stereotype(profile=profile)
+act['action1']['execution1']['tgv'] = core.TaggedValue(value="dummy value")
+act['action1']['lpc1'] = activities.PreConstraint(specification='True is True')
+act['action1']['lpo1'] = activities.PostConstraint(specification='False is False')
 
-act['action2'] = mm.OpaqueAction()
-act['action3'] = mm.OpaqueAction()
+act['action2'] = activities.OpaqueAction()
+act['action3'] = activities.OpaqueAction()
 
-act['join'] = mm.JoinNode()
-act['decision'] = mm.DecisionNode()
-act['merge'] = mm.MergeNode()
+act['join'] = activities.JoinNode()
+act['decision'] = activities.DecisionNode()
+act['merge'] = activities.MergeNode()
 
-act['flow end'] = mm.FlowFinalNode()
-act['end'] = mm.ActivityFinalNode()
+act['flow end'] = activities.FlowFinalNode()
+act['end'] = activities.ActivityFinalNode()
 
-act['1'] = mm.ActivityEdge(source=act['start'], target=act['fork'])
-act['2'] = mm.ActivityEdge(source=act['fork'], target=act['action1'])
-act['3'] = mm.ActivityEdge(source=act['fork'], target=act['action2'])
-act['4'] = mm.ActivityEdge(source=act['action1'], target=act['action3'])
-act['5'] = mm.ActivityEdge(source=act['action2'], target=act['join'])
-act['6'] = mm.ActivityEdge(source=act['action3'], target=act['decision'])
-act['7'] = mm.ActivityEdge(source=act['action3'], target=act['join'])
-act['8'] = mm.ActivityEdge(source=act['decision'], target=act['flow end'], guard="else")
-act['9'] = mm.ActivityEdge(source=act['decision'], target=act['merge'], guard="True")
-act['10'] = mm.ActivityEdge(source=act['join'], target=act['merge'])
-act['11'] = mm.ActivityEdge(source=act['merge'], target=act['end'])
+act['1'] = activities.ActivityEdge(source=act['start'], target=act['fork'])
+act['2'] = activities.ActivityEdge(source=act['fork'], target=act['action1'])
+act['3'] = activities.ActivityEdge(source=act['fork'], target=act['action2'])
+act['4'] = activities.ActivityEdge(source=act['action1'], target=act['action3'])
+act['5'] = activities.ActivityEdge(source=act['action2'], target=act['join'])
+act['6'] = activities.ActivityEdge(source=act['action3'], target=act['decision'])
+act['7'] = activities.ActivityEdge(source=act['action3'], target=act['join'])
+act['8'] = activities.ActivityEdge(source=act['decision'], target=act['flow end'], guard="else")
+act['9'] = activities.ActivityEdge(source=act['decision'], target=act['merge'], guard="True")
+act['10'] = activities.ActivityEdge(source=act['join'], target=act['merge'])
+act['11'] = activities.ActivityEdge(source=act['merge'], target=act['end'])
 
-mm.validate(model)
+activities.validate(model)
 #
